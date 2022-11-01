4bda9ffbbcce1069ba2ad366cc9f23aa08e6c7b8
LeeSan <skyer9@gmail.com>
2022-11-01 17:59:58 +0900

    bug fix

--- script/k8s-minikube.sh
+++ script/k8s-minikube.sh
@@ -3,4 +3,7 @@
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
 
+sudo usermod -aG docker $USER
+
+echo "exit and reconnect ssh"
 echo "Run : minikube start"
